89fa3ab35a8509cf567b2b9c359153e4dec012df
Daniel Sanz <daniel.sanz@unifr.ch>
2020-03-28 15:18:35 +0100

    minor updates.

--- lsmrn_study.py
+++ lsmrn_study.py
@@ -2,15 +2,19 @@ import numpy as np
 from itertools import count
 from matplotlib import pyplot as plt
 
-def rmse(y, y_pred):
-    return np.sqrt(np.mean(np.square(y - y_pred)))
+def rmse(y_true, y_pred):
+    # root mean square error
+    return np.sqrt(np.mean(np.square(y_true - y_pred)))
 
 def mape(y_true, y_pred):
+    # mean absolute percentage error
     return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
 
-# def autocorr(x):
-#     result = np.correlate(x, x, mode='full')
-#     return result[result.size // 2:]
+def center(dat):
+    # center the time series around 0
+    mean = np.mean(dat)
+    if mean > 0:
+        return dat - mean
 
 inputPath = "data/input/"
 outputPath = "data/output/"
@@ -18,15 +22,15 @@ outputPath = "data/output/"
 # PART I --------------------------------------------------------
 # ---------------------------------------------------------------
 
-# get first time series
-data = np.loadtxt(inputPath + "electricity_normal.txt")[:,0]
+# get one time series
+data = np.loadtxt(inputPath + "electricity_normal.txt")[:,11]
 
 # first, just take a look at the time series:
-# plt.plot(data)
-# plt.show()
+plt.plot(data)
+plt.show()
 
 # split into train and test
-SPLIT = 200
+SPLIT = 400
 train = data[0 : -SPLIT]
 
 # negative values are not supported, shift
@@ -35,7 +39,7 @@ if minTrain < 0:
     minTrain = abs(minTrain) + 1
     train += minTrain
 
-SNAP = 50
+SNAP = 200
 # split train data in list of snapshots. [[0:49], [50,99]....]
 snapshots = [train[x : x + SNAP] for x in range(0, len(train), SNAP)]
 
@@ -53,38 +57,43 @@ for i, G in enumerate(Gmats):
 # proximity matrix W using k-hop simil. (prox-hop)
 prox = 10
 Whop = sum([np.diag([1] * (SNAP - x), k=x) for x in range(prox)])
+
+# proximity matrix W using matrix profile.
 Wprof = None # soon
+
+# choose one of the above matrices
 W = Whop
 
 # PART IV -------------------------------------------------------
 # ---------------------------------------------------------------
 
 # constants
-Kdim = 30 # latent space dimension
+Kdim = 40 # latent space dimension
 lmbda = 0.001 # regularization param
-gamma = 2e-2 # regularization param
+gamma = 2e-3 # regularization param
 
 # Laplacian smoothing
 D = np.zeros((W.shape)) 
 np.fill_diagonal(D, np.sum(W,0))
 
-# initialisation of U_t, B and A randomly
+# initialisation of U_t, B and A
 Umats = [0.5 * (abs(np.zeros((SNAP, Kdim))) + 1)] * len(Gmats)
 
 A = 0.5 * (abs(np.zeros((Kdim, Kdim))) + 1)
 B = 0.5 * (abs(np.zeros((Kdim, Kdim))) + 1)
 
 # ---------------------------------------------------------------
-# update U matrices
+# global learning
 assert len(Ymats) == len(Gmats) and len(Gmats) == len(Umats)
 
 it = 0
-MAXITER = 600
+maxiter = 600
 converged = False
-while it < MAXITER and not converged:
+while it < maxiter and not converged:
     
     oldA = A.copy() # for convergence
-    
+
+    # update U matrices
     for t, Y, G, U in zip(count(), Ymats, Gmats, Umats):
         
         # in the pseudo-code U_t-1 and U_t+1 are not
@@ -100,8 +109,8 @@ while it < MAXITER and not converged:
     
     # ---------------------------------------------------------------
     # update B
-    utgu = lambda Uu, Gg, Yy: ((Uu.T).dot(Yy * Gg)).dot(Uu)
-    utuutu = lambda Bb: lambda Uu, Yy: Uu.T.dot(Yy * (Uu.dot(Bb).dot(Uu.T))).dot(Uu) # curry
+    utgu = lambda U, G, Y: ((U.T).dot(Y * G)).dot(U)
+    utuutu = lambda B: lambda U, Y: U.T.dot(Y * (U.dot(B).dot(U.T))).dot(U) # curry
     utubutu = utuutu(B)
 
     # formula [8]
@@ -116,32 +125,35 @@ while it < MAXITER and not converged:
     denoA = sum([Umats[i - 1].T.dot(Umats[i - 1]).dot(A) for i in range(1, len(Umats))])
     A = A * np.nan_to_num(np.divide(nomiA, denoA))
 
-    if it > 50 and it%2==0 and np.linalg.norm(oldA - A) <1e-4 :
+    if it > 50 and it % 2 == 0 and np.linalg.norm(oldA - A) < 1e-4 :
         print("converged in {} iterations".format(it))
         converged = True
+
     it = it + 1
 
 # PART V --------------------------------------------------------
 # ---------------------------------------------------------------
 
 # forecast
-Ulast = Umats[-2].copy()
+Ulast = Umats[-2]
 Acopy = A.copy()
 preds = None
 firstIt = True
+diag = 1 # wich diagonal we wanna extract
 for i in range(int(SPLIT/SNAP)):
+
     pred = (Ulast.dot(Acopy)).dot(B).dot((Ulast.dot(Acopy)).T)
     
     if firstIt:
         firstIt = False
-        preds = np.diag(pred, 0)
+        preds = np.diag(pred, diag)
     else:    
-        preds = np.concatenate(([preds, np.diag(pred, 0)]))
+        preds = np.concatenate(([preds, np.diag(pred, diag)]))
         Acopy = Acopy.dot(A)
 
-# unshift the values
-preds = preds - minTrain
-truth = data[-SPLIT:]
+# unshift or center the time series
+preds = center(preds)
+truth = data[-(SPLIT - diag * int(SPLIT/SNAP)):]
 
 #metrics
 print("rmse {}".format(rmse(truth,preds)))
@@ -149,5 +161,6 @@ print("mape {}".format(mape(truth,preds)))
 
 #plots
 plt.plot(truth, 'b', label='truth')
-plt.plot(preds, 'r', label="pred")
+plt.plot(preds, 'r', label='pred')
+plt.legend(loc="upper left")
 plt.show()
